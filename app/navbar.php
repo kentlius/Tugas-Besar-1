cdba03f037972bb38e9541509050b3163b06b777
Aldwin <13520167@std.stei.itb.ac.id>
2022-10-25 23:30:22 +0700

    feat: add navbar

---
+++ app/navbar.php
@@ -0,0 +1,30 @@
+<?php
+    function navbar() {
+        echo "<div class='navbar'>
+                <div class='navbar-left'>
+                    <a href='/'><img src='assets/Spotify_Logo_RGB_White.png' alt='logo' width='128'></a>
+                </div>
+                <div class='navbar-rightend'>
+                    <div class='navbar-middle'>
+                        <a href='/'>
+                            Home
+                        </a>
+                        <a href='/'>
+                            Search
+                        </a>
+                        <a href='/'>
+                            Lib
+                        </a>
+                    </div>
+                    <div class='navbar-right'>
+                        <a href='signup.php'>
+                            <button class='signup-btn'>Sign Up</button>
+                        </a>
+                        <a href='login.php'>
+                            <button class='login-btn'>Login</button>
+                        </a>
+                    </div>
+                </div>
+            </div>";
+    }
+?>
