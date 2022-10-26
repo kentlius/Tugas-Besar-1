bfb5b4894037688bec381447cf6b09b21091e0a1
Aldwin <13520167@std.stei.itb.ac.id>
2022-10-26 21:41:38 +0700

    feat: add homepage

--- app/index.php
+++ app/index.php
@@ -1,6 +1,12 @@
 <?php
-require('template/navbar.php');
+    require('template/navbar.php');
+    require('connect.php');
+
+    $query = $conn->query("SELECT * FROM song ORDER BY tanggal_terbit DESC LIMIT 10");
+    $songs = $query->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
+
 <DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,6 +20,26 @@ require('template/navbar.php');
     <div class="top-container">
         <?php 
         navbar(); 
+
         ?>
+        <div class="main">
+            <h1>Binotify</h1>
+            <div id='song-container' class='song-container'>
+                <?php foreach ($songs as $song) : ?>
+                    <?php $album = $conn->query("SELECT judul FROM album WHERE album_id = " . $song['album_id'])->fetch(PDO::FETCH_ASSOC); ?>
+                    <div class='song'>
+                        <a href="song.php?id=<?php echo $song['song_id']; ?>">
+                            <img src=<?php echo $song['image_path']; ?> alt="song image">
+                            <h2><?php echo $song['judul']; ?></h2>
+                            <p><?php echo substr($song['tanggal_terbit'], 0, 4); ?></p>
+                            <p><?php echo $song['penyanyi']; ?></p>
+                            <p><?php echo $album['judul'] ?></p>
+                        </a>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
     </div>
 </body>
+</html>
+
