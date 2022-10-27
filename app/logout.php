5bb807b586e056ab699dee2500adda92992d5947
kentliu <13520069@std.stei.itb.ac.id>
2022-10-27 23:45:34 +0700

    fix: logout redirect sesuai spek

--- app/logout.php
+++ app/logout.php
@@ -2,5 +2,5 @@
 session_start();
 session_unset();
 if (session_destroy()) {
-    header("Location: login.php");
+    header("Location: " . $_SERVER['HTTP_REFERER']);
 }
--- app/session/admin_auth.php
+++ app/session/admin_auth.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
 if (!isset($_SESSION['admin'])) {
-    header("Location: /");
+    header("Location: login.php");
     exit();
 }
