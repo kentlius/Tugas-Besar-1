a230058e675dbe5f58b1c56b2ff0fd0f2469c068
kentliu <13520069@std.stei.itb.ac.id>
2022-11-28 20:21:35 +0700

    feat: handle post premium audio file

---
+++ app/add-premium-song.php
@@ -0,0 +1,19 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$uploadErr = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $target_dir_audio = "uploads/audio/";
+    $target_file_audio = $target_dir_audio . basename($_FILES["audio_path"]["name"]);
+
+    if (file_exists($target_file_audio)) {
+        $uploadErr = "song already exists.";
+    } else {
+        if (move_uploaded_file($_FILES["audio_path"]["tmp_name"], $target_file_audio)) {
+            $uploadErr = "The file ". htmlspecialchars(basename($_FILES["audio_path"]["name"])). " has been uploaded.";
+        } else {
+            $uploadErr = "Sorry, there was an error uploading your file.";
+        }
+    }
+    echo $uploadErr;
+}
