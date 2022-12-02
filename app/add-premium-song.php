56a06dd6791fa210836854a854c526ba7f36fbcc
Vionie Novencia Thanggestyo <13520006@std.stei.itb.ac.id>
2022-12-02 14:52:34 +0700

    fix: cors premium song

--- app/add-premium-song.php
+++ app/add-premium-song.php
@@ -1,5 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Credentials: true");
 
 $uploadErr = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
